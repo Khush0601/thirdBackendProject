9fab938cc52262196015f9694f2eff0c5c083409
khushboo kumari <ksingh200601@gmail.com>
2024-11-29 00:27:15 +0530

    order model created

---
+++ Models/order.model.js
@@ -0,0 +1,63 @@
+const mongoose=require("mongoose")
+
+const orderSchema=new mongoose.Schema({
+productId:{
+    type:mongoose.SchemaTypes.ObjectId,
+    ref:'product',
+    required:true
+},
+userId:{
+    type:mongoose.SchemaTypes.ObjectId,
+    ref:'user',
+    required:true
+},
+addressId:{
+    type:mongoose.SchemaTypes.ObjectId,
+    ref:'address',
+    required:true
+},
+paymentStatus:{
+     true:String,
+     enum:['pending','success','failure'],
+     default:'pending'
+},
+transactionId:{
+ type:String,
+ 
+},
+productName:{
+type:String,
+required:true
+},
+productDescription:{
+type:String,
+required:true
+},
+productImage:{
+type:String,
+
+},
+Amount:{
+type:String
+},
+quantity:{
+    type:String,
+    default:1
+},
+createdAt:{
+    type:Date,
+    default:()=>{
+        return Date.now()
+    },
+    immutable:true
+},
+updatedAt:{
+        type:Date,
+        default:()=>{
+            return Date.now()
+        }
+       
+    }
+
+})
+module.exports=mongoose.model('order',orderSchema)
